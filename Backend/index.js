1d50c36fccc4b1f990fcf218d890e7964d090c13
Harsh Topno <topnoharsh@gmail.com>
2022-10-09 22:39:46 +0530

    Minor Changes and Task Schema - Ruotes

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // * Available Routes.
 app.use("/api/auth", require("./routes/auth"));
-// app.use("/api/project", require("./routes/project"));
+app.use("/api/task", require("./routes/task"));
 
 app.listen(port, () => {
     console.log(
